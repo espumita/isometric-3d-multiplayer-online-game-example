8cb9e77dcfd10ddbc6e40e7fcaf8e1ba9abda272
Espumita <davidfalconnaranjo@gmail.com>
2020-06-29 00:08:36 +0100

    Add OrthographicPlayerTag

---
+++ Assets/OrthographicPlayerTag.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class OrthographicPlayerTag : MonoBehaviour {
+    private Transform playerCameraTransform;
+
+    private void Start() {
+        playerCameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate() {
+        transform.LookAt(transform.position + playerCameraTransform.rotation * Vector3.forward, playerCameraTransform.rotation * Vector3.up);
+    }
+
+}
+
