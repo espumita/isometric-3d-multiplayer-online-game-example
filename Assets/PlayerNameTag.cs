31622ff2eb7a25114ac2d62322bab2d94a9334e8
Espumita <davidfalconnaranjo@gmail.com>
2020-06-28 23:51:58 +0100

    Set Serialized NameTag

---
+++ Assets/PlayerNameTag.cs
@@ -0,0 +1,17 @@
+﻿
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameTag : MonoBehaviourPun {
+
+    [SerializeField] private TextMeshProUGUI NickName;
+
+    void Start() {
+        if (photonView.IsMine) {
+            NickName.text = PhotonNetwork.NickName;
+        } else {
+            NickName.text = photonView.Owner.NickName;
+        }
+    }
+}
