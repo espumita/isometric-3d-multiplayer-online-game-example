dc9235cfb8d57278e7b6567a40c54de5427f76dc
Espumita <davidfalconnaranjo@gmail.com>
2020-07-07 21:20:02 +0100

    Refactor: extract mehtod KeepTagIsometric

--- Assets/OrthographicPlayerTag.cs
+++ Assets/OrthographicPlayerTag.cs
@@ -8,7 +8,13 @@ public class OrthographicPlayerTag : MonoBehaviour {
     }
 
     private void LateUpdate() {
-        transform.LookAt(transform.position + playerCameraTransform.rotation * Vector3.forward, playerCameraTransform.rotation * Vector3.up);
+        KeepTagIsometric();
+    }
+
+    private void KeepTagIsometric() {
+        var cameraOrthographicForward = transform.position + playerCameraTransform.rotation * Vector3.forward;
+        var cameraOrthographicUp = playerCameraTransform.rotation * Vector3.up;
+        transform.LookAt(cameraOrthographicForward, cameraOrthographicUp);
     }
 
 }
