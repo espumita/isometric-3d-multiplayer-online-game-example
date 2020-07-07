28dbcdd84238589bbc45333db9df766ded2dab58
Espumita <davidfalconnaranjo@gmail.com>
2020-07-07 21:34:04 +0100

    Refactor: rename prop targetToFollow

--- Assets/CameraFollowTarget.cs
+++ Assets/CameraFollowTarget.cs
@@ -4,14 +4,14 @@ public class CameraFollowTarget : MonoBehaviour {
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    public Transform target;
+    public Transform targetToFollow;
 
     void FixedUpdate() {
         MakeSmoothCameraMoveToTarget();
     }
 
     private void MakeSmoothCameraMoveToTarget() {
-        var cameraNextPosition = target.position + offset;
+        var cameraNextPosition = targetToFollow.position + offset;
         var smoothedNextCameraPosition = Vector3.Lerp(transform.position, cameraNextPosition, smoothSpeed);
         transform.position = smoothedNextCameraPosition;
     }
